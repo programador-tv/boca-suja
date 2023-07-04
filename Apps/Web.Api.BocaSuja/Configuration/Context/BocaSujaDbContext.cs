e582cc65a3f9ed96813b3bd9d767eac746e5f1bf
Gabriel Messias Costa <gmessiasc@gmail.com>
2023-07-04 13:10:26 -0300

    (Gabriel Costa 04/07/2023) feat: 'BocaSujaDbContext', adding EF Tools, first Migration, ContextString and db connection

---
+++ Apps/Web.Api.BocaSuja/Configuration/Context/BocaSujaDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Core.BocaSuja.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Web.Api.BocaSuja.Configuration.Context.DbMapping;
+
+namespace Web.Api.BocaSuja.Configuration.Context;
+
+public class BocaSujaDbContext : DbContext
+{
+  public BocaSujaDbContext(DbContextOptions<BocaSujaDbContext> options) : base(options)
+  {
+
+  }
+
+  public DbSet<Incidencia> Incidencias { get; set; }
+
+  private readonly IncidenciaDbMapping IncidenciaMapping = new();
+
+  protected override void OnModelCreating(ModelBuilder modelBuilder)
+  {
+    base.OnModelCreating(modelBuilder);
+
+    IncidenciaMapping.Build(modelBuilder);
+  }
+}
