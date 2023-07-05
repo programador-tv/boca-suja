8c2643562f91433bd2ab188ea36c295eab3d68fe
Gabriel Messias Costa <gmessiasc@gmail.com>
2023-07-05 10:00:40 -0300

    (Gabriel Costa 05/07/2023) removing sonarlint

---
+++ Tests/UnitTests/Web.Api.BocaSuja/Context/DbMapping/IncidenciaDbMapping.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Tests.UnitTests.Web.Api.BocaSuja.Context.DbMapping;
+
+[TestFixture]
+public class IncidenciaDbMapping
+{
+
+    private ModelBuilder _modelBuilder;
+    private IncidenciaDbMapping _incidenciaDbMapping;
+
+    [SetUp]
+    public void Setup()
+    {
+        _modelBuilder = new ModelBuilder();
+        _incidenciaDbMapping = new IncidenciaDbMapping();
+    }
+
+}
